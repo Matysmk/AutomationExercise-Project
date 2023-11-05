09d08b7eab1f398921ac988e0719f6487a2054b5
Paweł Matysiuk <matysiuk.pawel@o2.pl>
2023-11-05 17:24:23 +0100

    cypress confing uploads

--- cypress.config.js
+++ cypress.config.js
@@ -4,6 +4,7 @@ module.exports = defineConfig({
   reporter: "cypress-mochawesome-reporter",
   e2e: {
     baseUrl: "https://automationexercise.com/",
+    specPattern: "cypress/e2e/**/*.spec.js",
     setupNodeEvents(on, config) {
       // implement node event listeners here
       require("cypress-mochawesome-reporter/plugin")(on);
